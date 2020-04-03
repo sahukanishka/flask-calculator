75cfd645ceb8f97eb0e94ade31f6af9f5498d8e7
kanishkasahu <kanishkasahu005@gmail.com>
2020-04-03 15:57:24 +0530

    adding html files

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+#from flask we are importing a flask app , render_templete,request
+from flask import Flask ,  render_template , request
+#declare the app
 app=Flask(__name__)
 
+#starting an app route which is '/'
 @app.route('/')
-def hello_world():
-    return 'Calculator App'
+#declaring the main function 
+def main():
+    return render_template('templete.html')
 
+
+
+
+
+
+
+
+#decalaring the debug mode on 
 if __name__=='__main__':
     app.debug=True
     app.run(host='0.0.0.0',port=5000)
